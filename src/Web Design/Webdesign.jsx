23fe8c0acd58678891b6f3dbaa381b45caa96778
Bnks10061998 <banakasu4scot@gmail.com>
2025-05-22 16:49:44 +0530

    change9

--- src/Web Design/Webdesign.jsx
+++ src/Web Design/Webdesign.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import Webdesign1 from './routes/Webdesign1' 
 import Webdesign2 from './routes/Webdesign2' 
 import Webdesign3 from './routes/Webdesign3'
-import Webdesign4 from '../../src/Web Design/routes/Webdesign4';
-// import Webdesign4 from './routes/Webdesign4'
+// import Webdesign4 from '../../src/Web Design/routes/Webdesign4';
+import Webdesign4 from './routes/Webdesign4'
 import Webdesign5 from './routes/Webdesign5'
 import Webdesign6 from './routes/Webdesign6'
 import Webdesign7 from './routes/Webdesign7'
