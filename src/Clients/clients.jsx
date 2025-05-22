c0ab40150de6fb5374e0e6b705c2f506c8db0f6c
Bnks10061998 <banakasu4scot@gmail.com>
2025-05-22 16:04:22 +0530

    changes4

--- src/Clients/clients.jsx
+++ src/Clients/clients.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-import Clients1 from './routes/Clients1.jsx';
-import Clients2 from '../Clients/routes/clients2.jsx';
-import Clients3 from '../Clients/routes/clients3.jsx';
+import Clients1 from '../Clients/routes/Clients1.jsx';
+import Clients2 from '../Clients/routes/Clients2.jsx';
+import Clients3 from '../Clients/routes/Clients3.jsx';
 
 
 const clients = () => {
