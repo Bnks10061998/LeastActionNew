6810ab776dc034eb47f54c38311b344156975f55
Bnks10061998 <banakasu4scot@gmail.com>
2025-05-22 16:53:20 +0530

    changes11

--- src/Web Design/Webdesign.jsx
+++ src/Web Design/Webdesign.jsx
@@ -3,7 +3,7 @@ import Webdesign1 from './routes/Webdesign1'
 import Webdesign2 from './routes/Webdesign2' 
 import Webdesign3 from './routes/Webdesign3'
 // import Webdesign4 from '../../src/Web Design/routes/Webdesign4';
-import Webdesign4 from './routes/Webdesign4'
+// import Webdesign4 from './routes/Webdesign4'
 import Webdesign5 from './routes/Webdesign5'
 import Webdesign6 from './routes/Webdesign6'
 import Webdesign7 from './routes/Webdesign7'
@@ -14,7 +14,7 @@ const Webdesign = () => {
       <Webdesign1 /> 
       <Webdesign2 />
       <Webdesign3 />
-      <Webdesign4 />
+      {/* <Webdesign4 /> */}
       <Webdesign5 />
       <Webdesign6 />
       <Webdesign7 />
