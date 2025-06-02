d79e0c8d6cdd78ae5ea6e3633798bb21b163a988
Bnks10061998 <banakasu4scot@gmail.com>
2025-06-02 12:30:52 +0530

    changes1

--- src/Components/Aboutus2.jsx
+++ src/Components/Aboutus2.jsx
@@ -5,21 +5,6 @@ function Aboutus2() {
   return (
     <div className='Aboutus-bg'>
       <div className='Aboutus-hd'>
-        <div class="row">
-          <div class="col-md-6 img">
-            <img src='http://kosuketechnologies.com/assets/img/abtus-img.svg' />
-          </div>
-          <div class="col-md-6 txt">
-            <h3>About Us</h3>
-            <h2>Least Action Delivers Strategic Innovation <br /> for Your Success</h2>
-            <p>Least Action Pvt Ltd, a young and creative web design and <br /> development company, offers a wide range of services in both <br />
-              functional and technical solutions. They interact regularly with clients, <br /> focusing on improving productivity through quality services, cost- <br />
-              effective solutions, and timely project delivery. The team provides <br /> custom web applications, API integration, and payment gateway <br />
-              integration. Their approach includes thorough project analysis, <br /> requirement identification, client- approved design, and comprehensive <br />
-              testing before implementation, ensuring successful enterprise solutions <br />
-              through innovation and collective knowledge.</p>
-          </div>
-        </div>
         <div className='abt-bt'>
           {/* <h1>Least Action at a Glance</h1>
           <p>Least Action Pvt Ltd is a young and a creative web design <br /> and web development company</p><br /> */}
