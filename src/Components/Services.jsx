22224768695e215b52cf9edd406b4658403bfd2f
Bnks10061998 <banakasu4scot@gmail.com>
2025-06-02 13:19:41 +0530

    chnages2

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 const Services = () => {
   return (
     <section className="max-w-[1200px] mx-auto px-5 py-16 animate-fadeIn">
